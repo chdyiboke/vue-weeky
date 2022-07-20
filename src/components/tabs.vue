01785aa3093740d33857c158135230268f63faa6
caiyi <caiyi@lixiang.com>
2022-07-20 11:07:07 +0800

    vue使用element-plus

---
+++ src/components/tabs.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+    <el-tab-pane label="User" name="first">
+      <el-popover
+        class="box-item"
+        effect="dark"
+        content="Top Left prompts info"
+        placement="top-start"
+      >
+        <el-button>first</el-button>
+      </el-popover>
+    </el-tab-pane>
+    <el-tab-pane label="Config" name="second">
+      <el-popover
+        class="box-item"
+        effect="dark"
+        content="Top Left prompts info"
+        placement="top-start"
+      >
+        <el-button>second</el-button>
+      </el-popover>
+    </el-tab-pane>
+    <el-tab-pane label="Role" name="third">Role</el-tab-pane>
+    <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
+  </el-tabs>
+</template>
+<script lang="ts" setup>
+import { ref } from "vue";
+import type { TabsPaneContext } from "element-plus";
+
+const activeName = ref("first");
+
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+  console.log(tab, event);
+};
+</script>
+<style>
+.demo-tabs > .el-tabs__content {
+  padding: 32px;
+  color: #6b778c;
+  font-size: 32px;
+  font-weight: 600;
+}
+</style>
