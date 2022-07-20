da7eabb1ad4da46bb8aee94b3fccbc8273ee9c9b
caiyi <caiyi@lixiang.com>
2022-07-20 16:48:24 +0800

    点击内容修改

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,29 +2,21 @@
   <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
     <el-tab-pane label="User" name="first">
       first
-    <el-tooltip placement="top">
-      <div style="width:100px; height:1000px;">1234567890 </div>
-      <template #content>
-        <div style="font-size:16px;">
-          SEC定义：通过统计多个(20个)台架在长时间正常使用过程中，出现<br>
-          稳定性问题的次数，衡量HM系统稳定性的方法<br>
-          叫做SEC(Stabilityevaluative criteria)。
-        </div>
-      </template>
-    </el-tooltip>
+      <el-tooltip placement="top">
+        <div style="width: 100px; height: 1000px">1234567890</div>
+        <template #content>
+          <div style="font-size: 16px">啦啦啦啦啦啦</div>
+        </template>
+      </el-tooltip>
     </el-tab-pane>
     <el-tab-pane label="Config" name="second">
       second
       <el-tooltip placement="top">
-      <div style="width:100px; height:1000px;">1234567890 </div>
-      <template #content>
-        <div style="font-size:16px;">
-          SEC定义：通过统计多个(20个)台架在长时间正常使用过程中，出现<br>
-          稳定性问题的次数，衡量HM系统稳定性的方法<br>
-          叫做SEC(Stabilityevaluative criteria)。
-        </div>
-      </template>
-    </el-tooltip>
+        <div style="width: 100px; height: 1000px">1234567890</div>
+        <template #content>
+          <div style="font-size: 16px">啦啦啦啦啦</div>
+        </template>
+      </el-tooltip>
     </el-tab-pane>
     <el-tab-pane label="Role" name="third">Role</el-tab-pane>
     <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
@@ -32,13 +24,13 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref } from 'vue';
 
-const activeName = ref("first");
-console.error(activeName)
+const activeName = ref('first');
+console.error(activeName);
 const handleClick = (tab, event) => {
-  console.log(tab, event)
-}
+  console.log(tab, event);
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
